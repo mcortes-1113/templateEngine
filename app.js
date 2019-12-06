8a8a9df5f7ba1f67475e8c7674bc35790a29c265
Mario Cortes <mcortes.1113@gmail.com>
2019-12-06 00:14:05 -0600

    code to create manager instance

--- app.js
+++ app.js
@@ -1,97 +1,60 @@
 const inquirer = require('inquirer');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const manager = require('./lib/manager');
 
 
+//data input
+//use inquirer to prompt user for data
+//prompt data for manager
+//prompt data for additional employees
+//assign data to html elements
 
-//create prompts for favorite color and username.
-inquirer
-  .prompt([
+//code to run data input for manager
+//code to run data input for 
 
-    // * Use the [Inquirer npm package](https://github.com/SBoudrias/Inquirer.js/) to 
-// prompt the user for their email, id, and specific information based on their role with the company.
-//  For instance, an intern may provide their school, whereas an engineer may provide their GitHub username.
-    {
+function getManager() {
+
+  inquirer
+    .prompt([
+      {
+          type: "input",
+          message: "What is your name?",
+          name: "name"
+      },
+      {
+          type: "input",
+          message: "What is your email?",
+          name: "email"
+      },
+      {
+          type: "input",
+          message: "What is your ID?",
+          name: "id"
+      },
+      {
         type: "input",
-        message: "What is your name?",
-        name: "name"
-    },
-    {
+        message: "What is your department?",
+        name: "department"
+      },
+      {
         type: "input",
-        message: "What is your email?",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "What is your ID?",
-        name: "id"
-    },
-    {
-        type: "input",// change to options
-        message: "What is your role?",
-        name: "role"
-    },
-    //Move to second prompt---------------------------
-    {
-      type: "input",
-      message: "What is your github user name?",
-      name: "username"
-    },
-    {
-      type: "input",
-      message: "What is your school?",
-      name: "school"
-    }
-  ])
-  //---------------------------------------------------
-  .then(function(response) {
-    name = response.name;
-    email = response.email;
-    id = response.id;
-    role = response.role;
-    username = response.username;
-    school = response.school;
-    //call next function------------------
-    console.log(response); //remove
-    console.log(name, email, id, role, username, school);//remove
-});
+        message: "How many directs?",
+        name: "directs"
+      }
+    ])
+      .then(function(response) {
+        const mgr = new manager(
+          response.name,
+          response.email,
+          response.id,
+          'manager',
+          response.department,
+          response.directs
+        )
+        console.log(mgr);
+  })
 
+}
 
-
-
-// * Your app will run as a Node CLI to gather information about each employee.
-
-// * Below is an example of what your application may look like. Remember, the styling is completely up to you so try to make it unique.
-
-// ![Employee Summary 1](./Assets/10-OOP-homework-demo-1)
-// ![Employee Summary 2](./Assets/10-OOP-homework-demo-2)
-
-// In the `Develop` folder, there is a `package.json`, so make sure to `npm install`.
-
-// The dependencies are, [jest](https://jestjs.io/) for running the provided tests, and [inquirer](https://www.npmjs.com/package/inquirer) for collecting input from the user.
-
-// There are also unit tests to help you build the classes necessary.
-
-// It is recommended that you follow this workflow:
-
-// 1. Run tests
-// 2. Create or update classes to pass a single test case
-// 3. Repeat
-
-// 🎗 Remember, you can run the tests at any time with `npm run test`
-
-// ### Hints
-
-// * Create multiple HTML templates for each type of user. For example, you could use the following templates:
-
-//   * `main.html`
-
-//   * `engineer.html`
-  
-//   * `intern.html`
-  
-//   * `manager.html`
-
-// * You will want to make your methods as pure as possible. This means try to make your methods simple so that they are easier to test.
-
-// * The different employee types should all inherit some methods and properties from a base class of `Employee`.
-
-// * In your HTML template files, you may want to add a placeholder character that helps your program identify where the dynamic markup begins and ends.
+getManager();
