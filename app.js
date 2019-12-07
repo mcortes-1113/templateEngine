f7a38494cd473f60d38b34c08348a47fa9faba1d
Mario Cortes <mcortes.1113@gmail.com>
2019-12-07 00:09:57 -0600

    modified inquirer to include conditional questions

--- app.js
+++ app.js
@@ -1,60 +1,129 @@
 const inquirer = require('inquirer');
-const engineer = require('./lib/engineer');
-const intern = require('./lib/intern');
-const manager = require('./lib/manager');
+// const engineer = require('./lib/engineer');
+// const intern = require('./lib/intern');
+// const manager = require('./lib/manager');
+// const html = require(`./lib/html.js`);
 
+var managersData = [];
+var engineersData = [];
+var internsData = [];
 
-//data input
-//use inquirer to prompt user for data
-//prompt data for manager
-//prompt data for additional employees
-//assign data to html elements
-
-//code to run data input for manager
-//code to run data input for 
-
-function getManager() {
-
+function addMember() {
   inquirer
-    .prompt([
-      {
-          type: "input",
-          message: "What is your name?",
-          name: "name"
-      },
-      {
-          type: "input",
-          message: "What is your email?",
-          name: "email"
-      },
-      {
-          type: "input",
-          message: "What is your ID?",
-          name: "id"
-      },
-      {
-        type: "input",
-        message: "What is your department?",
-        name: "department"
-      },
-      {
-        type: "input",
-        message: "How many directs?",
-        name: "directs"
-      }
-    ])
-      .then(function(response) {
-        const mgr = new manager(
-          response.name,
-          response.email,
-          response.id,
-          'manager',
-          response.department,
-          response.directs
-        )
-        console.log(mgr);
+  .prompt([
+    {
+      type: "list",
+      name: "addMember",
+      message: "Add another Team Member?",
+      choices: ['Yes', 'No'],
+    }
+  ])
+  .then(function(response) {
+    if (response.addMember === 'Yes') {
+      getMemberData();
+    }
+    else {
+      console.log(managersData.length + ' managers added');
+      console.log(managersData);
+      console.log(engineersData.length + ' engineers added');
+      console.log(engineersData);
+      console.log(internsData.length + ' interns added');
+      console.log(internsData);
+      
+    }
+    return
   })
+};
 
-}
+function getMemberData() {
+  inquirer
+  .prompt([
+    {
+        type: "input",
+        message: "Team Member's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Team Member's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Team Member's ID?",
+        name: "id"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Team Member's role?",
+        choices: ['Manager', 'Engineer', 'Intern'],
+    },
+    {
+      type: "input",
+      message: "What is your department?",
+      name: "department",
+      when: (answers) => answers.role === 'Manager'
+    },
+    {
+      type: "input",
+      message: "How many directs?",
+      name: "directs",
+      when: (answers) => answers.role === 'Manager'
+    },
+    {
+      type: "input",
+      message: "Team Member's GitHub?",
+      name: "github",
+      when: (answers) => answers.role === 'Engineer'
+    },
+    {
+      type: "input",
+      message: "Team Member's years of experience?",
+      name: "expYears",
+      when: (answers) => answers.role === 'Engineer'
+    },
+    {
+      type: "input",
+      message: "Intern's school?",
+      name: "school",
+      when: (answers) => answers.role === 'Intern'
+    },
+    {
+      type: "input",
+      message: "Intern's graduation year?",
+      name: "gradYear",
+      when: (answers) => answers.role === 'Intern'
+    },
+  ])
+  .then(function(response) {
+    if (response.role === 'Manager') {
+      managersData.push(response);
+      addMember();
+    }
+    else if (response.role === 'Engineer') {
+      engineersData.push(response);
+      addMember();
+    }
+    else {
+      internsData.push(response);
+      addMember();
+    }
+})};
 
-getManager();
+getMemberData();
+
+//       .then(function(response) {
+//         const newmgr = new manager(
+//           response.name,
+//           response.email,
+//           response.id,
+//           'manager',
+//           response.department,
+//           response.directs
+//         )
+//         newmgr.addMgr(newmgr);
+//         newmgr.addMgrEl();
+//   })
+
+// }
